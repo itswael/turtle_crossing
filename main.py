375b29aed5f21affb1927e8b425c8f5e77e39dbe
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-01-10 21:23:08 -0500

    added scoreboard functionality and speed up on level completion

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import time
 from turtle import Turtle, Screen
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
-player = Player()
+
 screen = Screen()
-car_manager = CarManager()
 screen.setup(width=600, height=600)
+scoreboard = Scoreboard()
+player = Player()
+car_manager = CarManager()
+
 screen.tracer(0)
 
 screen.listen()
@@ -18,8 +22,12 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     game_is_on = car_manager.detect_collision(player)
+    if not game_is_on:
+        scoreboard.game_over()
     if player.is_at_finish():
+        scoreboard.level_up()
         player.moves_to_start()
+        car_manager.level_up()
 
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,24 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 260)
+        self.level = 0
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over!", align="center", font=FONT)
+
+    def level_up(self):
+        self.clear()
+        self.level += 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", align="left", font=FONT)
