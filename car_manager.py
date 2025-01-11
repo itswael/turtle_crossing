1087a3c09c080904dfdbec0c19f8fa28d9df78c0
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-01-10 20:27:25 -0500

    Added car creation and motion

--- car_manager.py
+++ car_manager.py
@@ -1,6 +1,24 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.all_cars = []
+
+    def create_cars(self):
+        new_car = Turtle("square")
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        random_y = random.randint(-250, 250)
+        new_car.goto(300, random_y)
+        self.all_cars.append(new_car)
+        self.move_cars()
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.goto(car.xcor() - MOVE_INCREMENT, car.ycor())
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import time
 from turtle import Turtle, Screen
+from player import Player
+from car_manager import CarManager
 
+player = Player()
 screen = Screen()
+car_manager = CarManager()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_cars()
+
+
+screen.exitonclick()
